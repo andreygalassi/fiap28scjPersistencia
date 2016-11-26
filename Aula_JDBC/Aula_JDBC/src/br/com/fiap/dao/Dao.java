e15d1d7cb1b6c19e631d55f8e4faf930fc982c08
Andrey <andreygalassi@gmail.com>
2016-11-26 10:22:08 -0200

    criação do dao

--- Aula_JDBC/Aula_JDBC/src/br/com/fiap/dao/Dao.java
+++ Aula_JDBC/Aula_JDBC/src/br/com/fiap/dao/Dao.java
@@ -1,5 +1,29 @@
 package br.com.fiap.dao;
 
-public class Dao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public abstract class Dao {
+
+	//usada para manter a conexão com o banco de dados
+	protected Connection cn;
+	//usado para realizar operaçãoes no banco de dados
+	protected PreparedStatement stm;
+	//usado para referenciar consultas
+	protected ResultSet rs;
+	//string de conexao
+	private String url="jdbc:mysql://localhost:3306/dbpedidos";
+	
+	protected void abrirConexao() throws Exception{
+		Class.forName("com.mysql.jdbc.Driver");
+		cn = DriverManager.getConnection(url, "root", "fiap");
+	}
+	
+	protected void fecharConexao() throws Exception{
+		if (cn !=null && !cn.isClosed()){
+			cn.close();
+		}
+	}
 }
